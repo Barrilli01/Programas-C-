55f4049c2bfe73a75ccda02c586b28017ee85efd
Barrilli01 <gabrielbarrillicruz@gmail.com>
2023-11-02 20:10:38 -0300

    Projeto de Cadastro de pessoas com Vetor Struct e Métodos

---
+++ CadastroPessoas/MenuCadastro/Menu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuCadastro
+{
+
+    internal class Menu
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            Menu menu = new Menu();
+            int num;
+
+                do
+                {
+                    menu.MainMenu();
+                    num = int.Parse(Console.ReadLine());
+                    
+                    Console.Clear();
+
+                    switch (num)
+                    {
+                        case 0:
+                            Console.WriteLine("Encerrado");
+                            break;
+                        case 1:
+                            pessoa.Adicionar();
+                            break;
+                        case 2:
+                            pessoa.Listar();
+                            break;
+                        default:
+                            Console.WriteLine("Número digitado não identificado no menu");
+                            Console.WriteLine("Digite uma opção do menu!");
+                            num = int.Parse(Console.ReadLine());
+                            break;
+                    }
+
+                } while (num == 1 || num == 2);
+
+           
+            Console.ReadKey();
+        }
+
+        public void MainMenu()
+        {
+            Console.WriteLine("BEM VINDO AO MENU!");
+            Console.WriteLine("1 - PARA ADICIONAR PESSOA");
+            Console.WriteLine("2 - PARA LISTAR PESSOA");
+            Console.WriteLine("0 - PARA ENCERRAR SESSÃO");
+        }
+    }
+}
---
+++ CadastroPessoas/MenuCadastro/Pessoa.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuCadastro
+{
+    struct dadosPessoa
+    {
+        public string nome;
+        public int idade;
+        public int celular;
+
+        public void dadosP(string nome, int idade, int celular)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.celular = celular;
+        }
+        public void Info()
+        {
+            Console.WriteLine("   Nome : {0}", this.nome);
+            Console.WriteLine("   Idade : {0}", this.idade);
+            Console.WriteLine("   Celular : {0}", this.celular);
+        }
+    }
+
+    internal class Pessoa
+    {
+        public int totalP = 0;
+        dadosPessoa[] p = new dadosPessoa[10];
+
+        public void Adicionar()
+        {
+            string nome;
+            int idade;
+            int celular;
+
+            Console.WriteLine("Digite o nome da pessoa " + totalP);
+            nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Digite a idade da pessoa " + totalP);
+            idade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de celular da pessoa " + totalP);
+            celular = int.Parse(Console.ReadLine());
+
+            p[totalP].nome = nome;
+            p[totalP].idade = idade;
+            p[totalP].celular = celular;
+
+            totalP++;
+
+            int qtt = p.Length - totalP;
+            Console.WriteLine();
+            Console.WriteLine("Pessoa adicionada!");
+            Console.WriteLine("Você ainda pode adicionar mais " + qtt + " pessoas");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void Listar()
+        {
+            for (int i = 0; i < totalP; i++) {
+                
+                Console.WriteLine();
+                Console.WriteLine("--------------------");
+                Console.WriteLine();
+                p[i].Info();
+
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+       
+    }
+}
