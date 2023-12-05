7b66f12d0981470c1ae30b3b25d55f88ddcb974f
Barrilli01 <gabrielbarrillicruz@gmail.com>
2023-12-05 15:52:11 -0300

    Gets e Sets

---
+++ POO/Propriedades/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste t = new Teste();
+
+            Console.WriteLine("Digite seu primeiro nome");
+            t.Nome = Console.ReadLine();
+            Console.WriteLine("Digite sua idade");
+            t.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write(t.Nome + " " + t.Sobrenome);
+            Console.WriteLine(" tem " + t.Idade + " anos");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ POO/Propriedades/Teste.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades
+{
+    internal class Teste
+    {
+
+        private string _nome;
+        public string Sobrenome { get; } = "Barrilli";
+        private int _idade;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public int Idade
+        {
+            get { return _idade; }
+            set { 
+                if (value < 18)
+                {
+                    Console.WriteLine("Você é menor de idade, menor de 18 anos");
+                } else
+                {
+                    _idade = value;
+                }
+            }
+        }
+    }
+}
